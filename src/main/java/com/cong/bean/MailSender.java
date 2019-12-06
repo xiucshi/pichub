665861d74f19adea58cec9bbc6a800dabe4fa008
xiucshi <xiuc_shi@163.com>
2019-12-06 13:38:01 +0800

    lombok出错，重新安装了插件

--- src/main/java/com/cong/bean/MailSender.java
+++ src/main/java/com/cong/bean/MailSender.java
@@ -3,7 +3,6 @@ package com.cong.bean;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.spi.AbstractLogger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
--- src/main/java/com/cong/controller/UserController.java
+++ src/main/java/com/cong/controller/UserController.java
@@ -4,7 +4,6 @@ import com.cong.bean.MailSender;
 import com.cong.service.AuthService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
