b7c821784937c0f7bee4aa4cf98d9faf0c076efa
xiucshi <xiuc_shi@163.com>
2019-12-10 12:47:52 +0800

    解决跨域问题

---
+++ src/main/java/com/cong/config/CORSConfig.java
@@ -0,0 +1,20 @@
+package com.cong.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Date : 2019/12/9
+ * @Author : xiuc_shi
+ **/
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedHeaders("*");
+    }
+}
